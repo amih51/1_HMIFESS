40c68a03b2cfdde1cdd59276f5add3712f2a2bfb
Tuyulll <19623185@std.stei.itb.ac.id>
2024-08-24 23:50:36 +0700

    responsive create post button

--- src/components/create-post-btn.tsx
+++ src/components/create-post-btn.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import CreatePostModal from './create-post'; // Make sure to update the import path
+import { PencilIcon } from '@heroicons/react/24/outline';
 
 export default function CreatePostButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -40,11 +41,8 @@ export default function CreatePostButton() {
         onClick={handleOpenModal}
         className="md:flex mt-0 md:mt-8 items-center text-center px-6 py-2 rounded-3xl bg-green-700 text-white hover:bg-green-900 transition-colors duration-100 overflow-y-auto w-full"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-3 hidden xl:block ">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-</svg>
-
-        Buat Menfess
+        <PencilIcon className='w-6 h-6 md:mr-3'/>
+        <div className='hidden md:block'>Buat Menfess</div>
       </button>
 
       {isModalOpen && (
