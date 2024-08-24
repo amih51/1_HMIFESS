c6db7a2e9b931c0ea73fa2b8224ce74b62e75cf0
Tuyulll <19623185@std.stei.itb.ac.id>
2024-08-24 18:20:17 +0700

    Upload logo HMIFess!!!

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -18,13 +18,13 @@ export default function Landing() {
   }, [searchParams]);
 
   return (
-    <main className="flex items-center justify-center min-h-screen -mt-16 md:mt-0 md:-ml-64 z-10">
+    <main className="flex items-center justify-center h-screen -mt-16 md:mt-0 md:-ml-64 z-10">
       <div className="grid grid-rows-[auto_1fr] md:grid-cols-2 md:grid-rows-1 w-full h-full">
         <div className="relative h-32 md:h-full w-full">
           <img 
-            src="hmif-logo.svg" 
+            src="hmifess-logo-bg.svg" 
             alt="Logo HMIF" 
-            className="absolute w-16 h-16 left-10 top-10"
+            className="absolute w-32 left-10 top-10"
           />
           <img 
             src="landing.svg" 
@@ -32,14 +32,16 @@ export default function Landing() {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="flex items-center justify-center bg-white p-8">
-          <div className="p-2 md:p-4">
-            <h1 className="text-5xl font-bold mb-4">Your Space, Your Voice.</h1>
-            <h2 className="text-2xl font-semibold mb-8">HMIFess: Where HMIF Students Connect & Share</h2>
-            <p className="text-lg text-gray-600 text-justify">
-              Welcome to <span className="font-bold">HMIFess</span>, the exclusive online forum for HMIF students at ITB. Whether you want to share insights, ask questions, or just connect with fellow students, HMIFess is the place to be. Join our community, where your thoughts matter, and every post can spark a conversation.
-            </p>
-            <button onClick={() => signIn("google")} className="rounded-md text-black border-2 border-blue-400 py-2 px-4 mt-4 hover:bg-slate-100">
+        <div className="flex flex-col items-center justify-center bg-white p-8">
+          <div className="flex flex-col h-full max-w-full max-h-full overflow-auto">
+            <div>
+              <h1 className="text-5xl font-bold mb-4">Your Space, Your Voice.</h1>
+              <h2 className="text-2xl font-semibold mb-8">HMIFess: Where HMIF Students Connect & Share</h2>
+              <p className="text-lg text-gray-600 text-justify">
+                Welcome to <span className="font-bold">HMIFess</span>, the exclusive online forum for HMIF students at ITB. Whether you want to share insights, ask questions, or just connect with fellow students, HMIFess is the place to be. Join our community, where your thoughts matter, and every post can spark a conversation.
+              </p>
+            </div>
+            <button onClick={() => signIn("google")} className="rounded-md text-black border-2 border-ijoPrimary py-2 px-4 mt-4 hover:bg-slate-100">
               Sign In with Google
             </button>
             {error && <p className="text-red-600">{error}</p>}
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -13,9 +13,9 @@ export default function Nav() {
 
   return (
     <div className="fixed flex items-center justify-between md:block top-0 left-0 w-full md:w-64 h-16 md:h-screen bg-white">
-      <div className="p-6 md:h-1/5">
+      <div className="p-6 mx-6 md:h-1/5 md:pt-16">
         <Link href="/">
-          <h1 className="pl-6 text-2xl font-bold text-black">HMIFess</h1>
+          <img src="hmifess-logo.svg" alt="Logo HMIFess" className="w-32"/>
         </Link>
       </div>
       <nav className="flex flex-col flex-shrink-0 justify-between h-3/4 overflow-y-auto bg-white">
@@ -33,7 +33,7 @@ export default function Nav() {
             </Link>
           </li>
         </ul>
-        <div className="pt-4 pr-8 md:pr-0 mx-6">
+        <div className="pt-2 pr-8 md:pr-0 mx-6">
           <DisplayName />
         </div>
       </nav>
