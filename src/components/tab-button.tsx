15c8d5154083d36a5c06a9727745dfb6d361d2ab
Abdullah Farhan <farhanjunaedi213@gmail.com>
2024-08-24 15:43:19 +0700

    fix: update profile to use TypeScript

---
+++ src/components/tab-button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type TabButtonProps = {
+  active: boolean;
+  selectTab: (tabName: string) => void;
+  children: string | JSX.Element;
+};
+
+/**
+ * A tab button. Shows the tab name and changes color when clicked.
+ * @param {TabButtonProps} props
+ * @returns {JSX.Element}
+ */
+const TabButton = ({ active, selectTab, children }: TabButtonProps): JSX.Element => {
+  const buttonClasses = active
+    ? "text-white border-b-2 border-white px-5 py-2"
+    : "text-white text-opacity-50 px-5 py-2";
+
+  return (
+    <button
+      onClick={() => {
+        if (typeof children === 'string') {
+          selectTab(children);
+        } else {
+          console.error('Children must be a string');
+        }
+      }}
+      className={`mr-5 font-semibold ${buttonClasses}`}>
+      {children}
+    </button>
+  );
+};
+
+export default TabButton
