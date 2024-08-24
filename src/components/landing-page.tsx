5ae85f3fd119ebb1ac97bd1e704f76ec18562978
Tuyulll <19623185@std.stei.itb.ac.id>
2024-08-25 02:55:39 +0700

    styling landing page

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -37,20 +37,18 @@ export default function Landing() {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="flex flex-col items-center justify-center bg-white p-8">
-          <div className="flex flex-col h-full max-w-full max-h-full overflow-auto">
-            <div>
-              <h1 className="text-5xl font-bold mb-4">Your Space, Your Voice.</h1>
-              <h2 className="text-2xl font-semibold mb-8">HMIFess: Where HMIF Students Connect & Share</h2>
-              <p className="text-lg text-gray-600 text-justify">
-                Welcome to <span className="font-bold">HMIFess</span>, the exclusive online forum for HMIF students at ITB. Whether you want to share insights, ask questions, or just connect with fellow students, HMIFess is the place to be. Join our community, where your thoughts matter, and every post can spark a conversation.
-              </p>
-            </div>
-            <button onClick={() => signIn("google")} className="rounded-md text-black border-2 border-ijoPrimary py-2 px-4 mt-4 hover:bg-slate-100">
-              Sign In with Google
-            </button>
-            {error && <p className="text-red-600">{error}</p>}
+        <div className="flex flex-col items-start justify-center bg-white p-8 max-w-full max-h-full overflow-auto">
+          <div>
+            <h1 className="text-5xl font-bold mb-4">Your Space, Your Voice.</h1>
+            <h2 className="text-2xl font-semibold mb-8">HMIFess: Where HMIF Students Connect & Share</h2>
+            <p className="text-lg text-gray-600 text-justify">
+              Welcome to <span className="font-bold">HMIFess</span>, the exclusive online forum for HMIF students at ITB. Whether you want to share insights, ask questions, or just connect with fellow students, HMIFess is the place to be. Join our community, where your thoughts matter, and every post can spark a conversation.
+            </p>
           </div>
+          <button onClick={() => signIn("google")} className="rounded-md text-black border-2 border-ijoPrimary py-2 px-4 mt-4 hover:bg-slate-100">
+            Sign In with Google
+          </button>
+          {error && <p className="text-red-600">{error}</p>}
         </div>
       </div>
     </main>
