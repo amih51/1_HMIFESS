e24dfd51a197a9cfa343d86378257a87f8121615
Tuyulll <19623185@std.stei.itb.ac.id>
2024-08-25 04:10:45 +0700

    fix vercel

--- src/components/display-post.tsx
+++ src/components/display-post.tsx
@@ -31,41 +31,39 @@ const DisplayPost: React.FC<DisplayPostProps> = ({ posts }) => {
     return <LoaderBar />; 
   }
 
-const imageStyle = {
-  borderRadius: '50%',
-}
+  const imageStyle = {
+    borderRadius: '50%',
+  };
 
   return (
-    <ul className=''>
-      <li className="border-b-2 border-gray-500"></li>
+    <ul>
       {posts.map((post) => (
-      <a href={`/m/${post.id}`}>
+        <Link href={`/m/${post.id}`}>
         <li key={post.id} className='border-b-2 border-gray-500 py-3'>
           {/*Username or Anonymous & Profile Image*/}
           <div className="flex flex-row font-bold place-items-center">
             {post.isAnon ? (
               <>
-              <UserCircleIcon className="w-12 h-12 -translate-x-1"/>
-              <div className="flex flex-col">
-                <p className="ml-1">Warga Biasa</p>
-                {/*Updated At*/}
-                <p className="ml-1 text-[10px] text-gray-400 font-normal">{new Date(post.createdAt).toLocaleString()} <br /></p>
-              </div>
+                <UserCircleIcon className="w-12 h-12 -translate-x-1"/>
+                <div className="flex flex-col">
+                  <p className="ml-1">Warga Biasa</p>
+                  {/*Updated At*/}
+                  <p className="ml-1 text-[10px] text-gray-400 font-normal">{new Date(post.createdAt).toLocaleString()} <br /></p>
+                </div>
               </>
-              ) : 
-              (
-                <>
-              <Link href={`/profile/${post.user.id}`}>
-                <Image src={post.user.image} alt="profile photo" width={40} height={40} style={imageStyle} />
-              </Link>
-              <div className="flex flex-col">
-                <Link href={`/profile/${post.user.id}`} className="ml-3 hover:underline">
-                  {post.user.name}
+            ) : (
+              <>
+                <Link href={`/profile/${post.user.id}`}>
+                  <Image src={post.user.image} alt="profile photo" width={40} height={40} style={imageStyle} />
                 </Link>
-                <p className="ml-3 text-[10px] text-gray-400 font-normal">{new Date(post.createdAt).toLocaleString()} <br /></p>
-              </div>
+                <div className="flex flex-col">
+                  <Link href={`/profile/${post.user.id}`} className="ml-3 hover:underline">
+                    {post.user.name}
+                  </Link>
+                  <p className="ml-3 text-[10px] text-gray-400 font-normal">{new Date(post.createdAt).toLocaleString()} <br /></p>
+                </div>
               </>
-              )}
+            )}
           </div>
           {/*Category*/}
           <div className="mt-2 text-xs bg-yellow-400 px-3 py-1 rounded-full place-items-center text-white inline-block">
@@ -85,7 +83,7 @@ const imageStyle = {
           {/*Comments*/}
           {/* <Comments postId={post.id} /> */}
         </li>
-      </a>
+      </Link>
       ))}
     </ul>
   );
